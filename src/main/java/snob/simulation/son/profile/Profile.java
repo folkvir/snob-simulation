c837d9c1917bc18e6eae53014ff6573522e56579
Folkvir <folkvir@outlook.fr>
2018-05-28 22:30:57 +0200

    profile with scoring method done

--- src/main/java/snob/simulation/son/profile/Profile.java
+++ src/main/java/snob/simulation/son/profile/Profile.java
@@ -1,6 +1,7 @@
 package snob.simulation.son.profile;
 
 
+import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
@@ -12,13 +13,14 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Profile {
+    public int WEIGH_EQUIVALENCE = Integer.MAX_VALUE;
+    public int WEIGH_CONTAINMENT = 2;
+    public int WEIGH_SUBSET = 1;
+
     public List<Triple> tpqs;
+
     public Profile() {
         this.tpqs = new ArrayList<>();
-    }
-
-    private void extract(Query query) {
-
     }
 
     public void update(String query) {
@@ -33,6 +35,76 @@ public class Profile {
                 }
             }
         });
+    }
+
+    /**
+     * Score the provided profile among us
+     * high value means that the profile is very interesting
+     * @param p the profile to compare with.
+     * @return a score based triple pattern containment
+     */
+    public int score(Profile p) {
+        int score = 0;
+        boolean stop = false;
+        Iterator<Triple> it = p.tpqs.iterator();
+        while(!stop && it.hasNext()) {
+            Triple pt = it.next();
+            Iterator<Triple> ittpqs = this.tpqs.iterator();
+            while(!stop && ittpqs.hasNext()) {
+                Triple us = ittpqs.next();
+                if (this.equivalence(us, pt)) {
+                    // System.out.println("Equivalence.");
+                    stop = true; // we have the highest score, stop or it will cause an overflow
+                } else if (this.containment(us, pt)) {
+                    try {
+                        score = Math.addExact(score, WEIGH_CONTAINMENT);
+                    } catch (ArithmeticException e) {
+                        stop = true;
+                    }
+                    // System.out.println("Containment." + stop);
+                } else if (this.subset(us, pt)) {
+                    try {
+                        score = Math.addExact(score, WEIGH_SUBSET);
+                    } catch (ArithmeticException e) {
+                        stop = true;
+                    }
+                    // System.out.println("Subset." + stop);
+                }
+                // System.out.println("" + it.hasNext() + ittpqs.hasNext() + stop);
+            }
+        }
+        if(stop) {
+            return Integer.MAX_VALUE;
+        }
+        return score;
+    }
+
+    public boolean equivalence(Triple tpa, Triple tpb) {
+        return tpa.equals(tpb);
+    }
+
+    public boolean containment(Triple tpa, Triple tpb) {
+        return this.contain(tpa.getSubject(), tpb.getSubject()) &&
+                contain(tpa.getPredicate(), tpb.getPredicate()) &&
+                contain(tpa.getObject(), tpb.getObject());
+    }
+
+    public boolean subset(Triple tpa, Triple tpb) {
+        return this.sub(tpa.getSubject(), tpb.getSubject()) &&
+                sub(tpa.getPredicate(), tpb.getPredicate()) &&
+                sub(tpa.getObject(), tpb.getObject());
+    }
+
+    public boolean contain(Node v1, Node v2) {
+       return this.eq(v1, v2) || ( !v1.isVariable() && v2.isVariable());
+    }
+
+    public boolean sub(Node v1, Node v2) {
+        return this.eq(v1, v2) || ( v1.isVariable() && !v2.isVariable());
+    }
+
+    public boolean eq(Node v1, Node v2) {
+        return v1.equals(v2);
     }
 }
 
--- src/test/java/snob/simulation/AppTest.java
+++ src/test/java/snob/simulation/AppTest.java
@@ -13,16 +13,7 @@ import snob.simulation.son.profile.Profile;
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-
-    /**
-     * Rigorous Test :-)
+     * Update fonction of profile should extract tpq
      */
     @Test
     public void profileShouldUpdateWithQuery()
@@ -35,5 +26,83 @@ public class AppTest
                 "   OPTIONAL { ?x foaf:nick ?nick }" +
                 "}");
         Assert.assertEquals(3, p.tpqs.size());
+    }
+
+    /**
+     * Update fonction of profile should extract tpq
+     */
+    @Test
+    public void profileScoringShouldReturnMaxValue()
+    {
+        String query = "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>"+
+                "SELECT DISTINCT ?name ?nick {" +
+                "   ?x foaf:mbox <mailt:person@server> ."+
+                "   ?x foaf:name ?name" +
+                "   OPTIONAL { ?x foaf:nick ?nick }" +
+                "}";
+        Profile p = new Profile();
+        p.update(query);
+        System.out.println(p.tpqs.toString());
+
+        Profile p2 = new Profile();
+        p2.update(query);
+        System.out.println(p2.tpqs.toString());
+
+        Assert.assertEquals(Integer.MAX_VALUE, p.score(p2));
+    }
+
+    /**
+     * Update fonction of profile should extract tpq
+     */
+    @Test
+    public void profileScoringShouldReturn2()
+    {
+        String query = "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>"+
+                "SELECT DISTINCT ?name ?nick {" +
+                "   ?x foaf:mbox <mailt:person@server> ."+
+                "   ?x foaf:name ?name" +
+                "   OPTIONAL { ?x foaf:nick ?nick }" +
+                "}";
+        String query2 = "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>"+
+                "SELECT DISTINCT ?name ?nick {" +
+                "  ?x ?p <mailt:person@server>"+
+                "}";
+        Profile p = new Profile();
+        p.update(query);
+        System.out.println(p.tpqs.toString());
+
+        Profile p2 = new Profile();
+        p2.update(query2);
+        System.out.println(p2.tpqs.toString());
+
+        Assert.assertEquals(2, p.score(p2));
+    }
+
+    /**
+     * Update fonction of profile should extract tpq
+     */
+    @Test
+    public void profileScoringShouldReturn3()
+    {
+        String query = "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>"+
+                "SELECT DISTINCT ?name ?nick {" +
+                "   ?x foaf:mbox <mailt:person@server> ."+
+                "   ?x foaf:name ?name" +
+                "   OPTIONAL { ?x foaf:nick ?nick }" +
+                "}";
+        String query2 = "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>"+
+                "SELECT DISTINCT ?name ?nick {" +
+                "  ?x ?p <mailt:person@server> ."+
+                "  ?x foaf:name \"toto\" "+
+                "}";
+        Profile p = new Profile();
+        p.update(query);
+        System.out.println(p.tpqs.toString());
+
+        Profile p2 = new Profile();
+        p2.update(query2);
+        System.out.println(p2.tpqs.toString());
+
+        Assert.assertEquals(3, p.score(p2));
     }
 }
