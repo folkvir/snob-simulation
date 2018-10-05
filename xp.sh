fc0f1c4cfbaae72a24371b0882ca87142ac92293
Grall Arnaud <folkvir@outlook.fr>
2018-10-05 17:24:43 +0200

    parallel execution

--- xp.sh
+++ xp.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 sh install.sh
 
-mvn exec:java@snob-50
-mvn exec:java@snob-100
-mvn exec:java@snob-200
-mvn exec:java@snob-50-rps
-mvn exec:java@snob-100-rps
-mvn exec:java@snob-200-rps
+mvn exec:java@snob-50 &
+mvn exec:java@snob-100 &
+mvn exec:java@snob-200 &
+mvn exec:java@snob-50-rps &
+mvn exec:java@snob-100-rps &
+mvn exec:java@snob-200-rps &
+wait
